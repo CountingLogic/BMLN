74d0e1f7d590f0c49c3da0c17335136e05b2d0f6
sagarAI <sagarmalhotra0594@gmail.com>
2019-02-26 17:05:26 +0100

    -MLM Pandas

--- MLN_Pandas.py
+++ MLN_Pandas.py
@@ -4,18 +4,21 @@ import itertools
 import operator as op
 IMPLIES = lambda x,y : ~(op.and_(x,~y))
 
+
+
 Persons = ['A', 'B', 'C']
 Smokes = ["S"+ x for x in Persons]
 Cancer  = ["C"+ x for x in Persons]
 Freinds = ["F" + "".join(a) for a in itertools.combinations(Persons, 2)]
 Groundings  = Smokes + Cancer + Freinds
-print(Groundings)
 
 df = pd.DataFrame(list(itertools.product([False, True], repeat = len(Groundings))), columns=Groundings)
 
+
+#Running loop over persons to define columns, not time consuming only trivial naming
 for person in Persons :
     df["SIC_"+ person] = IMPLIES(df["S"+person],df["C"+ person])
-    df.loc[df["SIC_"+ person]==True,["SIC_"+ person]] = 1
+    df.loc[df["SIC_"+ person]==True,["SIC_"+ person]] = 1.5
     df.loc[df["SIC_"+ person]==False,["SIC_"+ person]] = 0
 
 
@@ -29,7 +32,9 @@ for Freindship in Freinds:
 #df.loc[df["FIS_BC"]==True,["FIS_BC"]] = 100
 
 
+#Initiating a column for probability to zero 
 df['FIS_P']=0
+
 for Freindship in Freinds:
 
     df['FIS_P'] = df["FIS_" + Freindship[1:3]] + df['FIS_P']
@@ -37,6 +42,8 @@ for Freindship in Freinds:
 df['FIS_P'] = np.exp(df['FIS_P'])
 
 df['SIC_P'] = 0
+
+
 for person in Persons:
     df['SIC_P'] = df["SIC_" + person] + df['SIC_P']
 
@@ -44,16 +51,18 @@ df['SIC_P'] = np.exp(df['SIC_P'].astype(float))
 
 df['Potential'] = df['FIS_P'] * df['SIC_P']
 
+#Power of pandas : THis step is much faster then looping 
 Z = df['Potential'].sum()
 
 df['P']  =  df['Potential']/Z 
 
-print(df)
+#print(df)
 
 print(df['P'].sum())
 
 print(df['P'].min())
 
+#Again loop is not running on cells but columns, the cells are operated on in a vectorised manner 
 for Freindship in Freinds:
 
     df["FIC_"+ Freindship[1:3]] = IMPLIES(df[Freindship],(IMPLIES(df["C"+Freindship[1:2]], df["C"+ Freindship[2:3]]) & IMPLIES(df["C"+Freindship[2:3]], df["C"+ Freindship[1:2]])))
@@ -68,3 +77,4 @@ df['FIC_AB_P'] = df['P']*df['FIC_AB']
 
 print(df['FIC_AB_P'].sum())
 
+#print(df['FIC_AC'])
